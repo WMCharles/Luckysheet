a78c0be29e2aaf90fc6291c9e0ede0468d3d723f
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-25 13:57:14 +0300

    add protection

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,20 +18,49 @@ const options = {
             "defaultColWidth": 73, //Customized default column width
             "celldata": [], //Initial the cell data
             "config": {
-                "merge":{}, //merged cells
-                "rowlen":{}, //Table row height
-                "columnlen":{}, //Table column width
-                "rowhidden":{}, //hidden rows
-                "colhidden":{}, //hidden columns
-                "borderInfo":{}, //borders
-                "authority":{}, //Worksheet protection
+                "merge": {}, //merged cells
+                "rowlen": {}, //Table row height
+                "columnlen": {}, //Table column width
+                "rowhidden": {}, //hidden rows
+                "colhidden": {}, //hidden columns
+                "borderInfo": {}, //borders
+                "authority": {
+                    selectLockedCells: 1, //Select locked cells
+                    selectunLockedCells: 1, //Select unlocked cells
+                    formatCells: 1, //Format cells
+                    formatColumns: 1, //Format columns
+                    formatRows: 1, //Format rows
+                    insertColumns: 1, //Insert columns
+                    insertRows: 1, //Insert rows
+                    insertHyperlinks: 1, //Insert hyperlinks
+                    deleteColumns: 1, //Delete columns
+                    deleteRows: 1, //Delete rows
+                    sort: 1, //Sort
+                    filter: 1, //Filter
+                    usePivotTablereports: 1, //Use Pivot Table reports
+                    editObjects: 1, //Edit objects
+                    editScenarios: 1, //Edit scenarios   
+                    sheet: 1, //If it is 1 or true, the worksheet is protected; if it is 0 or false, the worksheet is not protected.
+                    hintText: "", //The text of the pop-up prompt
+                    algorithmName: "None",//Encryption scheme: MD2,MD4,MD5,RIPEMD-128,RIPEMD-160,SHA-1,SHA-256,SHA-384,SHA-512,WHIRLPOOL
+                    saltValue: null, //The salt parameter for password decryption is a random value set by yourself
+
+                    allowRangeList: [{ //Range protection
+                        name: "area", //Name
+                        password: "1", //Password
+                        hintText: "", //Prompt text
+                        algorithmName: "None",//Encryption scheme: MD2,MD4,MD5,RIPEMD-128,RIPEMD-160,SHA-1,SHA-256,SHA-384,SHA-512,WHIRLPOOL
+                        saltValue: null, //The salt parameter for password decryption is a random value set by yourself
+                        sqref: "$C$1:$D$5" //Protected range
+                    }],
+                }, //Worksheet protection
             },
             "scrollLeft": 0, //Left and right scroll bar position
             "scrollTop": 315, //Up and down scroll bar position
             "luckysheet_select_save": [], //selected area
             "calcChain": [],//Formula chain
-            "isPivotTable":false,//Whether is pivot table
-            "pivotTable":{},//Pivot table settings
+            "isPivotTable": false,//Whether is pivot table
+            "pivotTable": {},//Pivot table settings
             "filter_select": {},//Filter range
             "filter": null,//Filter configuration
             "luckysheet_alternateformat_save": [], //Alternate colors
@@ -39,8 +68,8 @@ const options = {
             "luckysheet_conditionformat_save": {},//condition format
             "frozen": {}, //freeze row and column configuration
             "chart": [], //Chart configuration
-            "zoomRatio":1, // zoom ratio
-            "image":[], //image
+            "zoomRatio": 1, // zoom ratio
+            "image": [], //image
             "showGridLines": 1, //Whether to show grid lines
         },
         {
@@ -62,7 +91,7 @@ const options = {
             "config": {},
         }
     ]
-    
+
 }
 
 // Initialize the table
